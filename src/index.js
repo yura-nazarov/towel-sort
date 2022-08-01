216ed8cef8d6e61ed41161f98ee0bcab9fe35151
Yuri <yuriy.nazaro@gmail.com>
2022-08-01 22:55:31 +1100

    fix: all done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+module.exports = function towelSort(matrix) {
+    const result = [];
+    if (!(matrix)) return result;
+    matrix.forEach((item, index) => {
+        if (index % 2 === 0) return result.push(...item);
+        result.push(...item.reverse());
+    });
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+    return result;
 }
